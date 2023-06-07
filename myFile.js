0ec49224592e719bfd216766f1fb803c317f3edf
asaqa988 <asaqa001@gmail.com>
2023-06-07 21:53:40 +0300

    update

---
+++ myFile.js
@@ -0,0 +1,110 @@
+
+
+
+
+
+
+
+
+function showMsg(){
+
+
+    let myStudents = [ firstStudent = {
+        name : "ahmad",
+        id : 33310 ,
+        age : 20 ,
+        brothers : ["mohammad","anas","omar"]
+    },
+    
+     secondStudent = {
+        name : "fatima",
+        id : 1121 ,
+        age : 24 ,
+        brothers: ["rami","fadi","shadi"]
+    },
+    
+    thirdStudent = {
+        name : "John",
+        id : 3322 ,
+        age : 22 ,
+        brothers: ["James", "Emma"]
+    },
+    
+    forthStuden = {
+        name : "Alice",
+        id : 3231 ,
+        age : 23 ,
+        brothers: ["Benjamin"]
+    },
+    
+    fifthStudent = {
+        name : "Michael",
+        id :778  ,
+        age :  27,
+        brothers: ["Ava","Jacob"]
+    },
+    
+    sixthStudent = {
+        name : "Emily",
+        id : 6689 ,
+        age : 30 ,
+        brothers: ["Mia", "William"]
+    },
+    seventhStudent = {
+        name :"mahmoud",
+        id : 4091,
+        age : 24 ,
+        brothers : null 
+
+    },
+    eighthStudent = {
+        name :"esraa",
+        id : 4091,
+        age : 24 ,
+        brothers : null 
+
+    },
+
+    ninthStudent = {
+        name :"marwa",
+        id : 5100,
+        age : 24 ,
+        brothers : null 
+
+    },
+    
+    
+    
+    
+    ]
+    
+    for(let i = 0 ; i < myStudents.length;i++){
+    
+       if(myStudents[i].id>2000){
+        console.log("good job"+" "+myStudents[i].name+" "+"you will be graduated")
+       }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+ 
+  
